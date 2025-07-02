7ecb96a519e620e2d818f966d0d85a332091eafa
Pradip Jadhav <jadhavpradip90@gmail.com>
2025-07-02 11:21:40 +0530

    React Router Tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,47 +1,34 @@
 import {
-  Form,
+  Outlet,
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
+  redirect
 } from "react-router";
 import type { Route } from "./+types/root";
-
 import appStylesHref from "./app.css?url";
+import { createEmptyContact } from "./data";
 
-export default function App() {
+export async function action() {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export function HydrateFallback() {
   return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              aria-label="Search contacts"
-              id="q"
-              name="q"
-              placeholder="Search"
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
+
+// existing imports and exports
+
+export default function App() {
+  return <Outlet />;
+}
+
 
 // The Layout component is a special export for the root route.
 // It acts as your document's "app shell" for all route components, HydrateFallback, and ErrorBoundary
